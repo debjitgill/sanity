c8330fad501300552b07a7f41a488d3519bf0731
debjitgill <debjit@gillbroking.com>
2023-12-05 18:12:29 +0530

    text-left

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -46,7 +46,7 @@ export default async function SlugPage({
           </div>
 
           <div>
-            <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+            <h1 className="text-3xl text-left font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
               {data.title}
             </h1>
           </div>
